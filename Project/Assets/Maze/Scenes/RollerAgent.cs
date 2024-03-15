70e166d66c425bbbe3e15c9cb1b992cfe7b5b91b
Tony Ly <54997152+lyt4atwit@users.noreply.github.com>
2024-03-14 21:41:04 -0400

    Initial commit

---
+++ Project/Assets/Maze/Scenes/RollerAgent.cs
@@ -0,0 +1,123 @@
+
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class AgentCode : Agent
+{
+
+    Rigidbody rBody;
+    public Transform Target;
+    public float forceMultiplier = 10;
+    public float prevDistanceToTarget = 100;
+    public float step_punishment = -0.001f;
+    public int steps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        // If the Agent fell, zero its momentum
+        if (this.transform.localPosition.y < 0)
+        {
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 2f, 0);
+        }
+
+        // Move the target to a new spot
+        // Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            AddReward(-5.0f);
+            //Debug.Log("HIT WALL");
+        }
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = new Vector3();
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        // Debug.Log(actionBuffers.ContinuousActions);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Reached target
+        if (distanceToTarget < 1.0f)
+        {
+            AddReward(10.0f);
+            EndEpisode();
+            prevDistanceToTarget = 100;
+            this.transform.localPosition = new Vector3(0, 2f, 0);
+        }
+
+        // Failed to reach target after 10000 steps
+        else if (steps > 10000)
+        {
+            this.transform.localPosition = new Vector3(0, 2f, 0);
+            EndEpisode();
+            prevDistanceToTarget = 100;
+        }
+
+        // Penalize for step
+        AddReward(step_punishment);
+
+        // Penalize/award for distance
+        AddReward(prevDistanceToTarget - distanceToTarget);
+
+        // Set previous distance
+        prevDistanceToTarget = distanceToTarget;
+
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+        
+        /*        Debug.Log("Horizontal input: " + Input.GetAxis("Horizontal"));
+                Debug.Log("Vertical input: " + Input.GetAxis("Vertical"));*/
+    }
+
+
+    //public float speed = 10.0f;
+
+    void FixedUpdate()
+    {
+        // rBody.AddForce(new Vector3(1, 0, 1) * forceMultiplier);
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rBody.AddForce(movement * speed);*/
+    }
+}
+
+// 
